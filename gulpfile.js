296ee769a65c4d953cd6172d9252762773efc448
one95nick <97886108+one95nick@users.noreply.github.com>
2022-05-24 18:58:23 +0500

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -126,6 +126,7 @@ const reload = (done) => {
 
 const watcher = () => {
   gulp.watch('source/sass/**/*.scss', gulp.series(styles));
+  gulp.watch('source/**/*.html', gulp.series(html));
   gulp.watch('source/js/*.js', gulp.series(scripts));
   gulp.watch('source/*.html').on('change', browser.reload);
 }
