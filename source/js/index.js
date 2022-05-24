6e5d33fa60cd3a391d42643ac7b63bff994f960f
one95nick <97886108+one95nick@users.noreply.github.com>
2022-05-24 18:56:45 +0500

    защита два

--- source/js/index.js
+++ source/js/index.js
@@ -1,4 +1,6 @@
 const header = document.querySelector('header');
 const headerToggle = document.querySelector('.header__toggle');
 
+header.classList.remove('no-js');
+
 headerToggle.addEventListener('click', () => header.classList.toggle('is-active'));
